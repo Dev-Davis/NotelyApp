54f4099597eef2a6614da72d06cde28df20705b9
dev_davis <sean.dev.davis@gmail.com>
2021-05-23 00:29:29 -0500

    added properties to the model

--- NotelyApp/Models/NoteModel.cs
+++ NotelyApp/Models/NoteModel.cs
@@ -7,5 +7,11 @@ namespace NotelyApp.Models
 {
     public class NoteModel
     {
+        public Guid Id { get; set; }
+        public string Subject { get; set; }
+        public string Details { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime LastModifiedDate { get; set; }
+        public bool IsDeleted { get; set; }
     }
 }
